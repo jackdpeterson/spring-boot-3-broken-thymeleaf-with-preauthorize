89709e94d92681e9388b199572ebe91b76d1362a
jackdpeterson <jack.peterson@gmail.com>
2023-03-21 00:21:29 -0700

    also broken for jdbc on 3.0.4

--- src/main/java/com/smbdevops/brokenpreauthorize/controller/IndexController.java
+++ src/main/java/com/smbdevops/brokenpreauthorize/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,14 +16,12 @@ public class IndexController {
 
     @GetMapping("")
     @PreAuthorize("hasRole('USER')")
-    public Mono<String> indexAction(final Model model) {
-        return this.repository.findByUsername("exists")
-                .doOnNext(usr -> {
-                    model.addAttribute("usr", usr);
-                })
-                .switchIfEmpty(this.repository.save(PrincipalEntity.builder()
-                        .username("exists")
-                        .profileDescription("in the database").build()))
-                .thenReturn("index");
+    public String indexAction(final Model model) {
+        final PrincipalEntity usr = this.repository.findByUsername("exists").orElseGet(() -> this.repository.save(PrincipalEntity.builder()
+                .username("exists")
+                .profileDescription("in the database").build()));
+
+        model.addAttribute("usr", usr);
+        return "index";
     }
 }
